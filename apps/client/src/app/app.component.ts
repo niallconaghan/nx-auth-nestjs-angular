c4218562705cf287b92f3aa1e046b01c967ade35
Niall <niall.conaghan@gmail.com>
2020-10-27 19:49:34 +0000

    feat: ✨generated nx workspace, nestjs api and angular client

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-auth-nestjs-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
