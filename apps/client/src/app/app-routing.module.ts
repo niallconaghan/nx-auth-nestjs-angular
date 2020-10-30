9408666db35930a6cf5d96ab3e149050ceac4391
Niall <niall.conaghan@gmail.com>
2020-10-30 14:52:12 +0000

    feat(client):  🖥 added authentication module and routing

---
+++ apps/client/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'authentication',
+    pathMatch: 'full'
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ apps/client/src/app/authentication/authentication.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationComponent } from './authentication.component';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+
+
+@NgModule({
+  declarations: [AuthenticationComponent],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ]
+})
+export class AuthenticationModule { }
