4cc6d694f5de89659caffbd73f1cc1cdc17cff73
Niall <niall.conaghan@gmail.com>
2020-11-03 23:26:17 +0000

    feat: 🧑 client login and logout

--- apps/client/src/app/authentication/authentication.component.ts
+++ apps/client/src/app/authentication/authentication.component.ts
@@ -1,5 +1,7 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AuthenticationResponse } from './interfaces/auth-response';
 import { AuthenticationService } from './services/authentication.service'
 @Component({
   selector: 'nx-auth-nestjs-angular-authentication',
@@ -21,15 +23,25 @@ export class AuthenticationComponent implements OnInit {
       username: this.username,
       password: this.password
     });
+
+    if(this.authenticationService.isAuthenticated()) {
+      this.formGroup.disable();
+    }
+
   }
 
   login(): void {
-    this.authenticationService.login(this.formGroup.value).subscribe((res) => {
-      console.log(res);
+    this.authenticationService.login(this.formGroup.value).subscribe((res: AuthenticationResponse) => {
+      this.formGroup.disable();
     });
   }
 
   register(): void {
     
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
+    this.formGroup.enable();
   }
 }
--- apps/client/src/app/authentication/services/authentication.service.ts
+++ apps/client/src/app/authentication/services/authentication.service.ts
@@ -4,19 +4,32 @@ import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import { AuthenticationResponse } from '../interfaces/auth-response';
 import { Credentials } from '../interfaces/credentials';
-
+import { StorageService } from '../../core/services/storage.service';
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private storageService: StorageService) { }
 
   login(credentials: Credentials): Observable<AuthenticationResponse> {
-    return this.httpClient.post<AuthenticationResponse>(`${environment.apiUrl}/authentication/login`, credentials);
+    return this.httpClient.post<AuthenticationResponse>(`${environment.apiUrl}/authentication/login`, credentials)
+    .pipe(tap((res: AuthenticationResponse) => {
+      this.storageService.set('access_token', res.access_token);
+      this.storageService.set('refresh_token', res.refresh_token);
+    }))
   }
 
   register(credentials: Credentials): Observable<AuthenticationResponse> {
     return null;
+  }
+
+  logout(): void {
+    this.storageService.clear();
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.storageService.get('access_token');
   }
 }
