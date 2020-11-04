56b4f5d5247624b8fa2fe8d8d66de6c3b5512a5d
Niall <niall.conaghan@gmail.com>
2020-11-04 09:07:34 +0000

    feat: 🚪 added logout the to content page

--- apps/client/src/app/content/content.component.ts
+++ apps/client/src/app/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication/services/authentication.service';
 
 @Component({
   selector: 'nx-auth-nestjs-angular-content',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.authenticationService.logout();
+    this.router.navigate(['authentication']);
+  }
 }
